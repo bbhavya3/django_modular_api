4068ad714fd310f63047af5c1fce702ee37f8109
bbhavya3 <boyapatibhavya03@gmail.com>
2026-03-16 10:48:33 +0530

    fixed api urls

--- modular_system/urls.py
+++ modular_system/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.http import HttpResponse
 
 
 def home(request):
@@ -20,6 +21,7 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
+
 
 urlpatterns = [
     path('', home),
--- vendor/urls.py
+++ vendor/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from .views import VendorListCreate, VendorDetail
 
 urlpatterns = [
-    # GET -> list vendors
-    # POST -> create vendor
-    path('', VendorListCreate.as_view(), name='vendor-list-create'),
-
-    # GET -> vendor details
-    # PUT/PATCH -> update vendor
-    # DELETE -> delete vendor
-    path('<int:id>/', VendorDetail.as_view(), name='vendor-detail'),
+    path('', VendorListCreate.as_view()),
+    path('<int:id>/', VendorDetail.as_view()),
 ]
