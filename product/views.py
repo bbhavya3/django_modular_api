aebbf2012fe2d6e00a971e9bf83dca947a3369f7
bbhavya3 <boyapatibhavya03@gmail.com>
2026-03-16 08:45:14 +0530

    fixed product views

--- product/views.py
+++ product/views.py
@@ -9,22 +9,15 @@ from .serializers import ProductSerializer
 class ProductListCreate(APIView):
 
     def get(self, request):
-
         products = Product.objects.filter(is_active=True)
-
         serializer = ProductSerializer(products, many=True)
-
         return Response(serializer.data)
 
-
     def post(self, request):
-
         serializer = ProductSerializer(data=request.data)
 
         if serializer.is_valid():
-
             serializer.save()
-
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -33,60 +26,53 @@ class ProductListCreate(APIView):
 class ProductDetail(APIView):
 
     def get_object(self, id):
-
         try:
             return Product.objects.get(id=id)
-
         except Product.DoesNotExist:
             return None
 
-
     def get(self, request, id):
-
         product = self.get_object(id)
 
-        if not product:
-            return Response({"error": "Product not found"}, status=404)
+        if product is None:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = ProductSerializer(product)
-
         return Response(serializer.data)
 
-
     def put(self, request, id):
-
         product = self.get_object(id)
+
+        if product is None:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = ProductSerializer(product, data=request.data)
 
         if serializer.is_valid():
-
             serializer.save()
-
             return Response(serializer.data)
 
         return Response(serializer.errors)
 
-
     def patch(self, request, id):
-
         product = self.get_object(id)
+
+        if product is None:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = ProductSerializer(product, data=request.data, partial=True)
 
         if serializer.is_valid():
-
             serializer.save()
-
             return Response(serializer.data)
 
         return Response(serializer.errors)
 
-
     def delete(self, request, id):
-
         product = self.get_object(id)
 
-        product.delete()
+        if product is None:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        return Response(status=204)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
