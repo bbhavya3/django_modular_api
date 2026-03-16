49bd878745a74c78b0ee5c546bd023493511833c
bbhavya3 <boyapatibhavya03@gmail.com>
2026-03-16 11:38:02 +0530

    fixed course views

--- course/views.py
+++ course/views.py
@@ -9,22 +9,52 @@ from .serializers import CourseSerializer
 class CourseListCreate(APIView):
 
     def get(self, request):
-
         courses = Course.objects.all()
-
         serializer = CourseSerializer(courses, many=True)
-
         return Response(serializer.data)
 
-
     def post(self, request):
-
         serializer = CourseSerializer(data=request.data)
 
         if serializer.is_valid():
-
             serializer.save()
-
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CourseDetail(APIView):
+
+    def get_object(self, id):
+        try:
+            return Course.objects.get(id=id)
+        except Course.DoesNotExist:
+            return None
+
+
+    def get(self, request, id):
+        course = self.get_object(id)
+
+        if not course:
+            return Response({"error": "Course not found"}, status=404)
+
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+
+    def put(self, request, id):
+        course = self.get_object(id)
+
+        serializer = CourseSerializer(course, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
+
+
+    def delete(self, request, id):
+        course = self.get_object(id)
+        course.delete()
+        return Response(status=204)
